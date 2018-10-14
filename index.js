9a7658d07b5043934314ee1a43822411f74a74b6
lucas <hey@lucascarl.com>
2018-10-14 16:36:21 +0200

    check for mixed delimiters in mac string

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var macRegex = require('mac-regex')
 
+var availableDelimiters = [':', '-']
+
+var hasMixedDelimiters = function (mac, delimiters) {
+  for (var i = 0; i < delimiters.length; i++) {
+    if (mac.includes(delimiters[i]) && mac.split(delimiters[i]).length !== 6) {
+      return true
+    }
+  }
+
+  return false
+}
+
 module.exports = function (mac) {
   if (typeof mac !== 'string') {
     throw new TypeError('is-mac expected a string')
+  }
+
+  if (hasMixedDelimiters(mac, availableDelimiters)) {
+    return false
   }
 
   return macRegex({ exact: true }).test(mac)
--- test.js
+++ test.js
@@ -1,26 +1,33 @@
 import test from 'ava'
 import isMac from './'
 
-test('returns true wtesth a mac', t => {
+test('returns true with a mac', t => {
   t.plan(1)
 
   t.true(isMac('aa-bb-cc-dd-ee-ff'))
-});
+})
 
-test('returns false wtesthout a mac', t => {
+test('returns false without a mac', t => {
   t.plan(1)
 
   t.false(isMac('aabb'))
-});
+})
 
 test('throws an error when a string is not passed', t => {
   t.plan(1)
 
   t.throws(isMac, TypeError)
-});
+})
 
 test('returns false when the string is not an exact match', t => {
   t.plan(1)
 
-  t.true(!isMac('apples aa:bb:cc:dd:ee:ff'));
-});
+  t.false(isMac('apples aa:bb:cc:dd:ee:ff'))
+})
+
+test('returns false when the string uses mixed delimiters', t => {
+  t.plan(2)
+
+  t.false(isMac('aa:bb:cc:dd:ee-ff'))
+  t.false(isMac('aa-bb-cc-dd:ee-ff'))
+})
